4bb3d38fc91e6512e4488c36c607e1624c0bf588
Sonam@Verma.com <Sonam@Verma.com>
2022-01-31 16:42:10 +0530

    username

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+include 'dbcon.php';
+if(isset($_POST['submit']))
+{
+$username = $_POST['username']; 
+$email= $_POST['email']; 
+$degree =$_POST['degree']; 
+$userampe = $_POST['username'];
+$username = $_POST['username'];
+
+print_r($file);
+}else{
+
+echo "No buttn has been clicked";
+}
