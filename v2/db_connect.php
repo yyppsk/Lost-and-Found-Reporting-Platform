e3f43eb30315a2492b01c49a1d7b597e353566dc
Pranjal Pratap Singh <69805496+yyppsk@users.noreply.github.com>
2023-06-29 23:35:13 +0530

    Update db_connect.php

--- v2/db_connect.php
+++ v2/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "123123@Blink";
+$password = "MyPassword";
 $dbname = "lostandfound";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or 
 die("Connection failed: " . mysqli_connect_error());
